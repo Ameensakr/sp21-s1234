31180137904e7ccb24fe526c40887412c01934ae
Ameensakr <ameensakr623@gmail.com>
2024-01-25 10:44:43 +0200

    done with Collatz

--- lab1/GetEnvironmentVariables.java
+++ lab1/GetEnvironmentVariables.java
@@ -3,10 +3,10 @@ import java.util.Properties;
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your sp21-s*** folder between the quotes
-    public static String REPO_DIR = "";
+    public static String REPO_DIR = "/home/amigoo/sp21-s1234";
 
     //  Fill in the path to your snaps-sp21-s*** folder between the quotes
-    public static String SNAPS_DIR = "";
+    public static String SNAPS_DIR = "/home/amigoo/snaps-sp21-s1234";
 
     // Fill in the type of your shell by running 'echo $0` in your terminal. It should be zsh or bash.
     public static String SHELL = "";
