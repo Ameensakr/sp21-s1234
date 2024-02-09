d45e0cdb6e5f4465382adba88c2dcb9344c5309b
Ameensakr <ameensakr623@gmail.com>
2024-02-09 18:06:13 +0200

    lab3 second subm

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -59,8 +59,9 @@ public class BuggyAList<Item> {
     /** Deletes item from back of the list and
       * returns deleted item. */
     public Item removeLast() {
-        if ((size < items.length / 4) && size > 8) {
-            resize(size / 4);
+
+        if ((size < items.length / 4)) {
+            resize(items.length / 2);
         }
         Item x = getLast();
         items[size - 1] = null;
