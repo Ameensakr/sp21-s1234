e00197aa708330a75c0f7b6744ba1134bd0c931f
Ameensakr <ameensakr623@gmail.com>
2024-02-12 21:47:58 +0200

    amazing

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,4 +1,5 @@
 package tester;
+
 import static org.junit.Assert.*;
 
 import edu.princeton.cs.introcs.StdRandom;
@@ -7,96 +8,87 @@ import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
     @Test
-    public void test1()
-    {
-        StudentArrayDeque<Integer> out = new StudentArrayDeque<>();
+    public void test1() {
+        ArrayDequeSolution<Integer> out = new ArrayDequeSolution<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
         int size = 0;
-        for (int i = 0; i < 1000; i++)
-        {
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < 2000; i++) {
             int type = StdRandom.uniform(4);
-            if(type == 0)
-            {
+
+            if (type == 0) {
                 // add last
                 size++;
                 int num = StdRandom.uniform(8000);
+                st.append("addLast(" + num + ')' + '\n');
                 out.addLast(num);
                 expected.addLast(num);
-            }
-            else if(type == 1)
-            {
-                size++;
+            } else if (type == 1) {
                 // add first
+                size++;
                 int num = StdRandom.uniform(8000);
+                st.append("addFirst(" + num + ')' + '\n');
                 out.addFirst(num);
                 expected.addFirst(num);
-            }
-            else if(type == 2)
-            {
-                if(size == 0)continue;
+            } else if (type == 2) {
+                if (size == 0) continue;
                 // remove first
+                size--;
                 Integer ex = expected.removeFirst();
                 Integer o = out.removeFirst();
-                assertEquals("addFirst(" + ex + ')' + '\n' +  "addFirst(" + ex + ')' + '\n' + "removeFirst(): " +
-                        o + '\n' + "removeFirst(): " +
-                        ex , ex , o);
-            }
-            else {
+                st.append("removeFirst()");
+                assertEquals(st.toString() , ex , o);
+            } else {
                 // remove last
-                if(size == 0)continue;
+                if (size == 0) continue;
+                size--;
                 Integer ex = expected.removeLast();
                 Integer o = out.removeLast();
-                assertEquals("addLast(" + ex + ')' + '\n' +  "addLast(" + ex + ')' + '\n' + "removeLast(): " +
-                        o + '\n' + "removeLast(): " +
-                        ex , ex , o);
+                st.append("removeLast()");
+                assertEquals( st.toString() , ex, o);
             }
         }
 
     }
 
     @Test
-    public void test2()
-    {
+    public void test2() {
         StudentArrayDeque<Integer> out = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
         int size = 0;
-        for (int i = 0; i < 2000; i++)
-        {
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < 2000; i++) {
             int type = StdRandom.uniform(4);
-            if(type == 0)
-            {
+
+            if (type == 0) {
                 // add last
+
                 size++;
                 int num = StdRandom.uniform(8000);
+                st.append("addLast(" + num + ')' + '\n');
                 out.addLast(num);
                 expected.addLast(num);
-            }
-            else if(type == 1)
-            {
+            } else if (type == 1) {
                 // add first
                 size++;
                 int num = StdRandom.uniform(8000);
+                st.append("addFirst(" + num + ')' + '\n');
                 out.addFirst(num);
                 expected.addFirst(num);
-            }
-            else if(type == 2)
-            {
-                if(size == 0)continue;
+            } else if (type == 2) {
+                if (size == 0) continue;
                 // remove first
                 Integer ex = expected.removeFirst();
                 Integer o = out.removeFirst();
-                assertEquals("addFirst(" + ex + ')' + '\n' +  "addFirst(" + ex + ')' + '\n' + "removeFirst(): " +
-                        o + '\n' + "removeFirst(): " +
-                        ex , ex , o);
-            }
-            else {
+                st.append("removeFirst()");
+                assertEquals(st.toString() , ex , o);
+            } else {
                 // remove last
-                if(size == 0)continue;
+                if (size == 0) continue;
                 Integer ex = expected.removeLast();
                 Integer o = out.removeLast();
-                assertEquals("addLast(" + ex + ')' + '\n' +  "addLast(" + ex + ')' + '\n' + "removeLast(): " +
-                        o + '\n' + "removeLast(): " +
-                        ex , ex , o);
+                st.append("removeLast()");
+                assertEquals( st.toString() , ex, o);
             }
         }
 
