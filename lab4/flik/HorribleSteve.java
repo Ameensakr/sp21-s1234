e6bbdac88f9dc34bd740260b22ed9d6b64f5b5c4
Ameensakr <ameensakr623@gmail.com>
2024-02-15 12:15:45 +0200

    i cant find bug

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,14 +1,30 @@
 package flik;
 
+import org.junit.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class HorribleSteve {
-    public static void main(String [] args) throws Exception {
+    @Test
+    public void test1() {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
-            if (!Flik.isSameNumber(i, j)) {
-                throw new Exception(
-                        String.format("i:%d not same as j:%d ??", i, j));
-            }
+                assertEquals("i:%d not same as j:%d ??" , i , j);
         }
         System.out.println("i is " + i);
     }
+    @Test
+    public void test2() {
+        int i = 1;
+        for (int j = 1; i < 500; ++i, ++j) {
+            assertEquals("i:%d not same as j:%d ??" , i , j);
+        }
+        System.out.println("i is " + i);
+    }
+
+
+
+
 }
