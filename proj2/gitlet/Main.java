262bad8451d8d16964de201326884936f070d9cb
Ameensakr <ameensakr623@gmail.com>
2024-10-09 13:16:31 +0300

    update:

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,5 +1,6 @@
 package gitlet;
 
+
 import java.io.IOException;
 
 /** Driver class for Gitlet, a subset of the Git version-control system.
@@ -10,7 +11,8 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
+
 
         if(args.length==0)
         {
@@ -19,7 +21,8 @@ public class Main {
         }
         String firstArg = args[0];
 
-        switch(firstArg) {
+
+        switch (firstArg) {
             case "init":
                 Repository.init();
                 break;
@@ -31,7 +34,7 @@ public class Main {
                 break;
 
             case "commit":
-                new Commit(args[1] , false);
+                new Commit(args[1], false);
                 break;
             case "log":
                 Repository.log();
@@ -48,12 +51,13 @@ public class Main {
                         Repository.checkout(args[2]);
                         break;
                     case 4:
-                        Repository.checkout(args[3],args[1]);
+                        Repository.checkout(args[3], args[1]);
                         break;
                     case 2:
                         break;
                 }
-
         }
+
+
     }
 }
